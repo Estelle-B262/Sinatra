17c2ed75e34459a9732d611002e96e5248bf86fd
ballifjbe <estelle.ballif@yahoo.com>
2019-10-21 16:25:19 +0200

    1er ajout E

---
+++ the_gossip_project_sinatra/lib/controller.rb
@@ -0,0 +1,18 @@
+require 'gossip'
+
+class ApplicationController < Sinatra::Base
+
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+
+  post '/gossips/new/' do
+    Gossip.new(params["gossip_author"], params["gossip_content"]).save
+    redirect '/'
+  end
+
+  get '/' do
+    erb :index, locals: {gossips: Gossip.all}
+  end
+
+end

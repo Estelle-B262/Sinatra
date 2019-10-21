27cef1c27e7d0f71509781aa5405e5bf17c631c5
ballifjbe <estelle.ballif@yahoo.com>
2019-10-21 17:26:53 +0200

    Ajout 2 E

--- the_gossip_project_sinatra/lib/controller.rb
+++ the_gossip_project_sinatra/lib/controller.rb
@@ -15,4 +15,11 @@ class ApplicationController < Sinatra::Base
     erb :index, locals: {gossips: Gossip.all}
   end
 
+  get '/gossips/:id' do
+    puts "Voici le numéro du potin que tu veux:#{params['id']}"
+    # matches "GET /hello/foo" and "GET /hello/bar"
+    # params['name'] is 'foo' or 'bar'
+    # erb :show
+  end
+
 end
